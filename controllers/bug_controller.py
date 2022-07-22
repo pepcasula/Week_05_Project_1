d084f50441426f50e02b900037df74a0227f6410
peppino <peppinocasula@yahoo.it>
2022-07-22 22:09:38 +0100

    fixed add_new_bug issue

--- controllers/bug_controller.py
+++ controllers/bug_controller.py
@@ -2,6 +2,7 @@ from crypt import methods
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.bug import Bug
+
 import repositories.bug_repository as bug_repository
 import repositories.product_repository as product_repository
 import repositories.ticket_repository as ticket_repository
@@ -29,8 +30,9 @@ def add_new_bug():
     bugname = request.form['short_name']
     bugdescription = request.form['description']
     bugproduct = request.form['product_id']
+    bugproduct_id = product_repository.select(bugproduct)
     bugdate = request.form['first_reported']
-    new_bug = Bug(bugname, bugdescription, bugproduct, bugdate)
+    new_bug = Bug(bugname, bugdescription, bugproduct_id, bugdate)
     bug_repository.save(new_bug)
     return render_template("bug_info/new.html", new_bug = new_bug)
 
